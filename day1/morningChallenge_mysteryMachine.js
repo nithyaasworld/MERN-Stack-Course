cd9bdc0fe48e72bb8400903bae3489af212a4286
Nithya <nithyaasworld@gmail.com>
2021-04-07 06:36:31 +0530

    refactored

--- day1/morningChallenge_mysteryMachine.js
+++ day1/morningChallenge_mysteryMachine.js
@@ -3,78 +3,46 @@ function mysteryMachine(grid, row, col, n, dir) {
     while (n > 0) {
         console.log('n is: ', n);
         console.log('grid is: ', grid);
+        console.log('row is: ', row, 'col is: ', col);
+        console.log('dir is: ', dir);
+        console.log("===========================================");
         if (grid[row][col] === 0) {
             grid[row][col] = 1;
-            dir = turnleft(dir);
-            if (grid[row + directionMap[dir][0]] && grid[row + directionMap[dir][0]][col + directionMap[dir][1]]) {
-                row = row + directionMap[dir][0];
-                col = col + directionMap[dir][1];
-            } else {
-                if (!grid[row + directionMap[dir][0]]) {
-                    let newrow = Array(grid[0].length).fill(0);
-                    console.log(newrow);
-                    if ((row + directionMap[dir][0]) >= 0) {
-                        grid.push(newrow);
-                    }else grid.unshift(newrow);
-                } else {
-                    if ((col + directionMap[dir][1]) >= 0) {
-                        for (let i = 0; i < grid.length; i++) {
-                            grid[i].push(0);
-                        }
-                    } else {
-                        for (let i = 0; i < grid.length; i++) {
-                            grid[i].unshift(0);
-                        }
-                    }
-                }
-                row = row + directionMap[dir][0];
-                col = col + directionMap[dir][1];
-            }
-            
+            dir = turnLeft(dir);            
         }else {
             grid[row][col] = 0;
-            dir = turnright(dir);
-            if (grid[row + directionMap[dir][0]] && grid[row + directionMap[dir][0]][col + directionMap[dir][1]]) {
-                row = row + directionMap[dir][0];
-                col = col + directionMap[dir][1];
+            dir = turnRight(dir);
+        }
+        //to move forward one unit 
+        if (grid[row + directionMap[dir][0]] && grid[row + directionMap[dir][0]][col + directionMap[dir][1]]) {
+            row = row + directionMap[dir][0];
+            col = col + directionMap[dir][1];
+        } else { // to expand a row or column when needed
+            if (!grid[row + directionMap[dir][0]]) {
+                let newrow = Array(grid[0].length).fill(0);
+                if ((row + directionMap[dir][0]) >= 0) grid.push(newrow);
+                else grid.unshift(newrow);
             } else {
-                if (!grid[row + directionMap[dir][0]]) {
-                    let newrow = Array(grid[0].length).fill(0);
-                    if ((row + directionMap[dir][0]) >= 0) {
-                        grid.push(newrow);
-                    }else grid.unshift(newrow);
+                if ((col + directionMap[dir][1]) >= 0) {
+                    for (let i = 0; i < grid.length; i++) {
+                        grid[i].push(0);
+                    }
                 } else {
-                    if ((col + directionMap[dir][1]) >= 0) {
-                        for (let i = 0; i < grid.length; i++) {
-                            grid[i].push(0);
-                        }
-                    } else {
-                        for (let i = 0; i < grid.length; i++) {
-                            grid[i].unshift(0);
-                        }
+                    for (let i = 0; i < grid.length; i++) {
+                        grid[i].unshift(0);
                     }
                 }
-                row = row + directionMap[dir][0];
-                col = col + directionMap[dir][1];
             }
+            row = row + directionMap[dir][0];
+            col = col + directionMap[dir][1];
         }
         n--;
     }
-    function turnleft(dir){
-        switch (dir) {
-            case 0 : return 3;
-            case 1: return 0;
-            case 2: return 1;
-            case 3 : return 2;
-        }
+    function turnLeft(dir){
+        return (dir - 1) < 0 ? 3 : (dir - 1);
     }
-    function turnright(dir){
-        switch (dir) {
-            case 0 : return 1;
-            case 1: return 2;
-            case 2: return 3;
-            case 3 : return 0;
-        }
+    function turnRight(dir){
+        return (dir + 1) > 3 ? 0 : (dir + 1);
     }
     return grid;
 }
