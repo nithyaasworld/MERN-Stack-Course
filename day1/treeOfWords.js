52e1261956164c7e83de8191b7677029e3bf99f9
Nithya <nithyaasworld@gmail.com>
2021-04-10 15:07:52 +0530

    pushing the work of friday from pc

--- day1/treeOfWords.js
+++ day1/treeOfWords.js
@@ -1,41 +1,43 @@
 // https://gist.github.com/McLarenCollege/49e2725953fa8abe2500a5d790fd6e8f
-
 function treeOfWords(inputStr){
+    let treeInLevelOrder = [inputStr];
+    for (let i = 0; i < treeInLevelOrder.length; i++){
+        if (treeInLevelOrder[i].length > 1) {
+            let left = treeInLevelOrder[i].slice(0, treeInLevelOrder[i].length / 2);
+            let right = treeInLevelOrder[i].slice(treeInLevelOrder[i].length / 2);
+            treeInLevelOrder.push(left);
+            treeInLevelOrder.push(right);
+        }
+    }
+    console.log(treeInLevelOrder);
+    let binaryTree = { value: treeInLevelOrder[0], left: null, right: null };
 
-    let preOrderBTList = [];
-    recursion(inputStr, preOrderBTList);
-    function recursion(inputStr, preOrderBTList){
-        let left = inputStr.slice(0, inputStr.length/2);
-        let right = inputStr.slice(inputStr.length/2);
-        preOrderBTList.push([left,right]);
-        
-            let innerLeft1 = left.slice(0, left.length/2);
-            let innerRight1 = left.slice(left.length/2);
-            
-            if(innerLeft1.length > 1) {
-                preOrderBTList.push([innerLeft1]);
-                recursion(innerLeft1, preOrderBTList);
-            } 
-            if(innerRight1.length > 1){
-                preOrderBTList.push([innerRight1]);
-                recursion(innerRight1, preOrderBTList);
-            }
-        
-        
-            let innerLeft2 = right.slice(0, right.length/2);
-            let innerRight2 = right.slice(right.length/2);
-           
-            if(innerLeft2.length > 1) {
-                preOrderBTList.push([innerLeft2]);
-                recursion(innerLeft2, preOrderBTList);
-            } 
-            if(innerRight2.length > 1){
-                preOrderBTList.push([innerRight2]);
-                recursion(innerRight2, preOrderBTList);
-            }    }
-    console.log(preOrderBTList);
+    let currNode = binaryTree;
+    let i = 0;
+    buildTree(currNode, i)
+    function buildTree(currNode, i) {
+        if (treeInLevelOrder[(2 * i) + 1]) {
+            currNode.left = { value: treeInLevelOrder[(2 * i) + 1], left: null, right: null };
+            buildTree(currNode.left, i + 1);
+        }
+        if (treeInLevelOrder[(2 * i) + 2]) {
+            currNode.right = { value: treeInLevelOrder[(2 * i) + 2], left: null, right: null };
+            buildTree(currNode.right, i + 2);
+        }
+    }
+    console.log(binaryTree);
+    print(binaryTree);
 
+    function print(tree) {     
+        let queue = [tree];
+        while (queue.length != 0) {
+            let node = queue.shift();
+            console.log(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
 }
 
-// treeOfWords("helo");
-treeOfWords("anjanakutty");
+//treeOfWords("hello");
+ treeOfWords("helo");
