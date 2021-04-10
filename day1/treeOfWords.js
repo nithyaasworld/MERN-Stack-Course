d5610180c196a1a7d1bae3b7478150438bba6ce1
Nithya <nithyaasworld@gmail.com>
2021-04-10 15:54:15 +0530

    refactored the code

--- day1/treeOfWords.js
+++ day1/treeOfWords.js
@@ -13,50 +13,24 @@ function treeOfWords(inputStr){
             treeInLevelOrder.push(null);
         }
     }
-    // console.log(treeInLevelOrder);
     let binaryTree = { value: treeInLevelOrder[0], left: null, right: null };
-
     let currNode = binaryTree;
     let i = 0;
     buildTree(currNode, i)
     function buildTree(currNode, i) {
-        if ((treeInLevelOrder[(2 * i) + 1] !== undefined) && (treeInLevelOrder[(2*i) + 1] !== null) ) {
+        if (treeInLevelOrder[(2 * i) + 1]) {
             currNode.left = { value: treeInLevelOrder[(2 * i) + 1], left: null, right: null };
-            // console.log('================================================');
-            // console.log('Inside left');
-            // console.log(' i is: ', i);
-            // console.log('currNode.value is: ', currNode.value);
-            // console.log('tree In Level Order is: ', treeInLevelOrder);
             treeInLevelOrder[(2*i) + 1] = null;
-            // console.log('tree In Level Order after making null is : ', treeInLevelOrder);
-            // console.log('================================================');
-            // console.log('\n\n');
             buildTree(currNode.left, (2*i) + 1);
         }
-        if ((treeInLevelOrder[(2 * i) + 2] !== undefined) && (treeInLevelOrder[(2 * i) + 2] !== null) ) {
+        if (treeInLevelOrder[(2 * i) + 2]) {
             currNode.right = { value: treeInLevelOrder[(2 * i) + 2], left: null, right: null };
-            // console.log('================================================');
-            // console.log('Inside right');
-            // console.log('currNode.value is: ', currNode.value);
-            // console.log('tree In Level Order is: ', treeInLevelOrder);
             treeInLevelOrder[(2*i) + 2] = null;
-            // console.log('tree In Level Order after making null is : ', treeInLevelOrder);
-            // console.log('================================================\n\n');
             buildTree(currNode.right, (2*i) + 2);
         }
     }
-    console.log(binaryTree);
-    print(binaryTree);
-
-    function print(tree) {     
-        let queue = [tree];
-        while (queue.length != 0) {
-            let node = queue.shift();
-            console.log(node.value);
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
-        }
-    }
+    return(binaryTree);
 }
 
-//treeOfWords("hello");
+console.log(treeOfWords("hello"));
+console.log(treeOfWords("helo"));
