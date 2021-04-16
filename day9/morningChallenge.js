97d9d556c78f037fbfe5944a8957e30510e49f05
Nithya <nithyaasworld@gmail.com>
2021-04-17 02:45:14 +0530

    dp solution added

--- day9/morningChallenge.js
+++ day9/morningChallenge.js
@@ -1,34 +1,39 @@
 function minStep(string1, string2) {
-    let subString1 = findallSubstring(string1);
-    let subString2 = findallSubstring(string2);
-    let commonsubString = commonLongestsubString(subString1, subString2);
-    while (commonsubString.length > 1) {
-        string1 = string1.replace(commonsubString, '');
-        string2 = string2.replace(commonsubString, '');
-        subString1 = findallSubstring(string1);
-        subString2 = findallSubstring(string2);
-        commonsubString = commonLongestsubString(subString1, subString2);
-    } 
-    console.log(string1, string2);
-  
-    function findallSubstring(input) {
-        let subString = [];
-        for (let i = 0; i < input.length - 1; i++){
-            for (let j = i + 1; j < input.length; j++){
-                subString.push(input.slice(i, j + 1));
-            }
+    let dp = Array.from(Array(string1.length), () => Array(string2.length).fill(null));
+    //filling the first row of dp
+    let currDistance = 0;
+    let letterFound = false;
+    for(let i=0; i<dp[0].length; i++){
+        if(!letterFound && string1[0] === string2[i]){
+            dp[0][i] = currDistance;
+            letterFound = true;
+        }else{
+            dp[0][i] = ++currDistance;
         }
-        return subString;
-        }
-    function commonLongestsubString(subString1, subString2) {
-        let longestCommonSubstring = '';
-        subString1.forEach(str => {
-            if (subString2.includes(str)
-                && str.length > longestCommonSubstring.length) {
-                longestCommonSubstring = str;
-            }
-        });
-        return longestCommonSubstring;
     }
+    //filling the first column of dp
+    currDistance = 0;
+    letterFound = false;
+    for(let i=0; i<dp.length; i++){
+        if(!letterFound && string1[0] === string2[i]){
+            dp[i][0] = currDistance;
+            letterFound = true;
+        }else{
+            dp[i][0] = ++currDistance;
+        }
+    }
+    //filling the rest of the states of dp
+    for(let i=1; i<dp.length; i++){
+        for(let j=1; j<dp[0].length; j++){
+            if(string1[i] === string2[j]){
+                dp[i][j] = dp[i-1][j-1];
+            }else{
+                dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j]) + 1;
+            }
+        }
+    }
+    return dp.slice(-1)[0].slice(-1)[0];
 }
-minStep('anshuman', 'antihuman');
+console.log(minStep('abad', 'abac')); // should return 1
+console.log(minStep('Anshuman', 'Antihuman')); // should return 2
+console.log(minStep('kitten', 'sitting')); // should return 3
