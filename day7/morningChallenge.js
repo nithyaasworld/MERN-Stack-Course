3563de8c67330b8247663a5b70527731ec5ff7af
Nithya <nithyaasworld@gmail.com>
2021-04-17 09:17:10 +0530

    solved again using dp

--- day7/morningChallenge.js
+++ day7/morningChallenge.js
@@ -1,35 +1,27 @@
 // https://gist.github.com/McLarenCollege/a7b63e9a3fa0b860365271a3b35c4455
-function numberOfPossibleIntegers(n) {
-
-    let array = Array(n).fill('1');
-    let str = array.join('');
-    let result = [str];
-    
-    let finalResult = [];
-    findAllPossibleCombination();
-    return new Set(finalResult).size + 1;
-
-    function findAllPossibleCombination() {
-        while (result.length > 0) {
-            let item = result.shift();
-            finalResult.push(item);
-            if (item.length === 2) continue;
-            let itemCombination = new Set();
-            item = item.split('');
-            for (let i = 0; i < item.length; i++){
-                let remaining = item.slice(0);
-                remaining.splice(i, 1);
-                for (let j = 0; j < remaining.length; j++){
-                    let combinations = remaining.slice(0);
-                    combinations[j] = (Number(combinations[j]) + Number(item[i])).toString();
-                    combinations.sort();
-                    itemCombination.add(combinations.join(''));
-                }
-            }
-            result.push(...Array.from(itemCombination));
-        }
+function findPartitions(n) {
+ let dp = Array.from(Array(n+1), () => Array(n+1).fill(null));
+ //Filling the first column
+ for(let i=0; i<n+1; i++){
+    dp[i][0] = 1;
+ }
+//Filling the first row
+ for(let i=1; i<n+1; i++){
+    dp[0][i] = 0;
+ }
+ //Filling the rest of the dp states
+ for(let i=1; i<n+1; i++){
+    for(let j=1; j<n+1; j++){
+       if(j < i){
+          dp[i][j] = dp[i-1][j];
+       }else {
+          dp[i][j] = dp[i-1][j] + dp[i][j-i];
+       }
     }
-}
-console.log(numberOfPossibleIntegers(4)); 
-console.log(numberOfPossibleIntegers(5));
-// console.log(numberOfPossibleIntegers(50));
+ }
+ return dp[n][n];
+ };
+ console.log(findPartitions(4)); //should return 5
+ console.log(findPartitions(5)); //should return 7
+ console.log(findPartitions(50)); //should return 204226
+ console.log(findPartitions(80)); //should return 15796476
