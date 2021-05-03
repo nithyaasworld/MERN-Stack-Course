a72258517c1cccc08d7921299c710b1426d7dedd
Nithya <nithyaasworld@gmail.com>
2021-05-03 08:58:09 +0530

    day 19 morning Challenge

---
+++ day19/morningChallenge.js
@@ -0,0 +1,15 @@
+function findMaxProfit(input) {
+    let i = 0;
+    let result = 0;
+    while (input[i + 1]) {
+        if (input[i] < input[i + 1]) {
+            result += input[i+1] - input[i];
+        }
+        i++;
+    }
+    return result;
+}
+console.log(findMaxProfit([5, 2, 10])); //should return 8
+console.log(findMaxProfit([1, 2, 3])); //should return 2
+console.log(findMaxProfit([5, 2, 10, 12, 8, 1])); //should return 10
+console.log(findMaxProfit([100, 50, 30, 20, 40])); //should return 20
