1b87807693264eb22ec55f1a13d0d71f7b8da712
Nithya <nithyaasworld@gmail.com>
2021-05-26 13:55:13 +0530

    done the day32 morning challenge

--- day32/morningChallenge.js
+++ day32/morningChallenge.js
@@ -1,40 +1,37 @@
+//Problem Statement https://gist.github.com/McLarenCollege/1f370cf08039d4448720fbd0748697d0
 function checkNums(arr, k) {
-    if (arr.length % 4 !== 0) return false;
+    if (arr.length % k !== 0) return false;
     let subArrayCount = arr.length / k;
-    let allCombinations = [];
-    allCombinations.push(Array.from(Array(subArrayCount), () => Array(1)));
-    
-    // while (allCombinations.every(item => item.every(subItem => subItem.length === k))) {
-    let subCombination = allCombinations.shift();
-    cons
-    
-    let unUsedElements = findUnusedElements(subCombination, arr.length);
-    console.log(unUsedElements);
-        // for (let i = 0; i < subCombinations.length;  i++){
-        //     for (let j = 0; j < subCombinations[0].length; j++){
-        //         unUsedElements.splice(unUsedElements.indexOf(subCombinations[i][j]), 1);
-        //     }
-        // }
-        // console.log(unUsedElements);
-        // for (let i = 0; i < arr.length; i++){
-        //     console.log(subCombinations);
-        // }
-    // }
-}
-function findUnusedElements(subCombination, lengthOfArray) {
-    let unUsedElements = new Array(lengthOfArray).fill().map((_, i) => i);
-    for (let i = 0; i < subCombination.length; i++){
-        for (let j = 0; j < subCombination[0].length; j++){
-            unUsedElements.splice(unUsedElements.indexOf(subCombination[i][j]), 1);
+    arr.sort((a, b) => a - b);
+    let obj = {};
+    for (let i = 0; i < arr.length; i++){
+        if (obj[arr[i]]) {
+            obj[arr[i]]++;
+            if (obj[arr[i]] > k) return false;
+        } else {
+            obj[arr[i]] = 1;
         }
     }
-    console.log(unUsedElements);
-    // let unUsedElements = new Array(arr.length).fill().map((_, i) => i);
-    // for (let i = 0; i < subCombinations.length;  i++){
-    //     for (let j = 0; j < subCombinations[0].length; j++){
-    //         unUsedElements.splice(unUsedElements.indexOf(subCombinations[i][j]), 1);
-    //     }
-    // }
-    // console.log(unUsedElements);
+    for (let i = 0; i < subArrayCount; i++){
+        let item = [];
+        for (const key in obj) {
+            item.push(key);
+            if (item.length > 1) {
+                if (item[item.length - 1] - item[item.length - 2] !== 1) return false;
+            }
+            obj[key] -= 1;
+            if (obj[key] === 0) {
+                delete obj[key];
+            }
+            if (item.length === k) {
+                break;
+            }
+        }
+        if (item.length !== k) return false;
+    }
+    return true;
 }
-console.log(checkNums([1, 2, 3, 3, 4, 4, 5, 6], 4));
+console.log(checkNums([1, 2, 3, 3, 4, 4, 5, 6], 4)); // expected: true
+console.log(checkNums([3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11], 3)); //expected: true
+console.log(checkNums([3, 3, 2, 2, 1, 1], 3)); //expected: true
+console.log(checkNums([3, 3, 2, 2, 1, 2], 3)); //expected: false
