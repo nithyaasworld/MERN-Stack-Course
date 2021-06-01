7e9a3c91c6ec37458ef8340d06b86b440758de29
Nithya <nithyaasworld@gmail.com>
2021-06-01 09:19:58 +0530

    formatted

--- day39/morningChallenge.js
+++ day39/morningChallenge.js
@@ -3,7 +3,6 @@ function printVertically(inputStr) {
     let strArr = inputStr.split(' ');
     let longestStr = strArr.reduce((acc, val) => val.length > acc ? val.length : acc, 0);
     let result = Array.from(Array(longestStr), () => new Array(strArr.length))
-
     for (let i = 0; i < strArr.length; i++){
         for (let j = 0; j < longestStr; j++){
             result[j][i] = strArr[i][j] || ' ';
