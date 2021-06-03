60183a7683033705fef36b6237e84bdaf5907cae
Nithya <nithyaasworld@gmail.com>
2021-06-03 18:15:56 +0530

    updated as per the mentoring session discussion

--- day41/morningChallenge.js
+++ day41/morningChallenge.js
@@ -1,30 +1,14 @@
 //https://gist.github.com/McLarenCollege/15bd572e41ab215939c2a66dc9fdaad3
 function nBonacciRatio(n) {
-    let previousTen = [0, 1];
-    for (let i = 2; i < 10; i++){
-        previousTen[i] = n * previousTen[i - 1] + previousTen[i - 2];
-    }
-    let ratioOfLastTen = [];
-    for (let i = 2; i < previousTen.length; i++){
-       ratioOfLastTen.push(previousTen[i] / previousTen[i - 1]);
-    }
-    while (!ratioOfLastTen.every(x => Number(x.toFixed(16)) === Number(ratioOfLastTen[0].toFixed(16)))) {
-        previousTen = previousTen.slice(-2);
-        for (let i = 2; i < 10; i++){
-            previousTen[i] = n * previousTen[i - 1] + previousTen[i - 2];
-        }
-        ratioOfLastTen = [];
-        findRatioofLastTen(previousTen);
-    }
-    function findRatioofLastTen(inputArr) {
-        for (let i = 2; i < inputArr.length; i++){
-            ratioOfLastTen.push(inputArr[i] / inputArr[i - 1]);
-         }
-    }
-    return ratioOfLastTen[0];
+    let x2 = 0, x1 = 1, x0 = n*x1 +x2, i=0;
+     do{
+        [x2, x1] = [x1, x0];
+         x0 = n * x1 + x2;
+    }while (x0 !== Infinity)
+    return x1 / x2;
 }
 console.log(nBonacciRatio(1)); //should return 1.61803
 console.log(nBonacciRatio(2)); //should return 2.4142
 console.log(nBonacciRatio(3)); // should return 3.302775637731995
 console.log(nBonacciRatio(7)); // should return 7.140054944640259
-console.log(nBonacciRatio(50)); //doesn't work for this 
+console.log(nBonacciRatio(50)); // returns 50.0199920063936
