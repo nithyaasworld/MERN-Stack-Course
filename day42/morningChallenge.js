70dd80277b22ca922525abd604199af497b562e1
Nithya <nithyaasworld@gmail.com>
2021-06-04 23:33:51 +0530

    returning the ball escaped location instead of -1

--- day42/morningChallenge.js
+++ day42/morningChallenge.js
@@ -5,42 +5,50 @@ function findBallFall(inputArr) {
         let result = 1;
         while (currPos[0] != inputArr.length) {
             [x, y] = currPos;
-            console.log('x and y are: ', x, y);
+            // console.log('x and y are: ', x, y);
             if (!inputArr[x]) {
                 console.log('line 10 executed');
                 result = -1; break;
             } else if (inputArr[x][y] === 1) {
-                console.log('line 13 executed');
+                // console.log('line 13 executed');
                 if (inputArr[x][y + 1] === 1) {
-                    console.log('line 15 executed');
+                    // console.log('line 15 executed');
                     currPos = [x + 1, y + 1];
                     continue;
                 } else {
-                    console.log('line 18 executed');
+                    // console.log('line 18 executed');
                     result = -1;
                     break;
                 }
             } else if (inputArr[x][y] === -1) {
-                console.log('line 22 executed');
+                // console.log('line 22 executed');
                 if (inputArr[x][y - 1] === -1) {
-                    console.log('line 25 executed');
-                    currPos = [x - 1, y];
+                    // console.log('line 25 executed');
+                    currPos = [x + 1, y -1];
                     continue;
                 } else {
-                    console.log('line 28 executed');
+                    // console.log('line 28 executed');
                     result = -1;
                     break;
                 }
             } else if (inputArr[x][y] === undefined) {
-                console.log('line 33 executed');
+                // console.log('line 33 executed');
                 result = -1;
                 break;
-            } 
+            }
         }
-        console.log("while loop is done");
-        resultArr.push(result);
+        // console.log("while loop is done");
+        if (result === -1) {
+            resultArr.push(result);
+        } else {
+            resultArr.push(currPos[1]);
+        }
     }
-    console.log(resultArr);
+    return resultArr;
 }
 grid = [[1, 1, 1, -1, -1], [1, 1, 1, -1, -1], [-1, -1, -1, 1, 1], [1, 1, 1, 1, -1], [-1, -1, -1, -1, -1]];
-findBallFall(grid); //should return [1,-1,-1,-1,-1]
+console.log(findBallFall(grid)); //should return [1,-1,-1,-1,-1]
+grid2 = [[-1]]; //should return [-1]
+console.log(findBallFall(grid2));
+grid3 = [[1, 1, 1, 1, 1, 1], [-1, -1, -1, -1, -1, -1], [1, 1, 1, 1, 1, 1], [-1, -1, -1, -1, -1, -1]];
+console.log(findBallFall(grid3)); //should return [0,1,2,3,4,-1]
