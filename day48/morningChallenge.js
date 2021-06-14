d322703e225f46b346a0aa2beb20c9f576291425
Nithya <nithyaasworld@gmail.com>
2021-06-14 08:48:35 +0530

    day48 morning Challenge done

---
+++ day48/morningChallenge.js
@@ -0,0 +1,22 @@
+//https://gist.github.com/McLarenCollege/ea9f791d2f07c03c1a2071c6d31fbc42
+
+function simplifiedFractions(n){
+    let result = [];
+    for(let denominator=2; denominator<=n; denominator++){
+        for(let numerator=1; numerator<=n-1; numerator++){
+            if(numerator < denominator){
+                if (numerator === 1){
+                    result.push(`${numerator}/${denominator}`);
+                }else if(denominator % numerator !== 0){
+                    result.push(`${numerator}/${denominator}`);
+                }
+            }
+        }
+    }
+    return result;
+}
+console.log(simplifiedFractions(4));
+console.log(simplifiedFractions(1));
+console.log(simplifiedFractions(3));
+console.log(simplifiedFractions(2));
+console.log(simplifiedFractions(5));
