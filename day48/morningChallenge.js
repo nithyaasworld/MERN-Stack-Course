25cfe4c612e3afc9c330237a068c42379140455c
Nithya <nithyaasworld@gmail.com>
2021-06-15 06:18:55 +0530

    fixed the issue with numbers highter than 5

--- day48/morningChallenge.js
+++ day48/morningChallenge.js
@@ -1,20 +1,25 @@
 //https://gist.github.com/McLarenCollege/ea9f791d2f07c03c1a2071c6d31fbc42
 
 function simplifiedFractions(n){
-    let result = [];
+    let result = {};
     for(let denominator=2; denominator<=n; denominator++){
         for(let numerator=1; numerator<denominator; numerator++){
                 if (numerator === 1){
-                    result.push(`${numerator}/${denominator}`);
+                    result[numerator/denominator] = `${numerator}/${denominator}`;
                 }else if(denominator % numerator !== 0){
-                    result.push(`${numerator}/${denominator}`);
+                    if (result[numerator/denominator]){
+                        continue;
+                    }else {
+                        result[numerator/denominator] = `${numerator}/${denominator}`;
+                    }
                 }
         }
     }
-    return result;
+    return Object.values(result);
 }
 console.log(simplifiedFractions(4));
 console.log(simplifiedFractions(1));
 console.log(simplifiedFractions(3));
 console.log(simplifiedFractions(2));
 console.log(simplifiedFractions(5));
+console.log(simplifiedFractions(6));
