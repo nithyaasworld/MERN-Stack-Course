3da6615505da653ed4aaf9b49ccbbf6ce6bf83a4
Nithya <nithyaasworld@gmail.com>
2021-06-30 10:15:43 +0530

    implemented the way2

--- day60/morningChallenge.js
+++ day60/morningChallenge.js
@@ -1,37 +1,22 @@
 function neededTime(n, headID, manager, informTime) {
-    // ---------- Way: 1 works only for the given Input  ------------- //
-        
-    // let neededTime = 0    manager = [... new Set(manager)];
-    // manager.forEach((manager, emp) => {
-    //     if (manager !== -1) {
-    //         neededTime += informTime[manager];
+    let neededTime = [];
+    for (let i = 0; i < n; i++){
+        neededTime.push(findInformTime(i));
+    }
+    return Math.max(...neededTime);
 
-    //     } 
-    // })
-    // return neededTime;
-
-    // ---------- Way: 2 Work in Progress ------------------ //
-
-    // let neededTime = [];
-
-    // for (let i = 0; i < n; i++){
-    //     neededTime.push(findInformTime(i));
-    // }
-
-    // console.log(neededTime);
-
-    // function findInformTime(empID) {
-    //     if (empID == headID) return 0;
-    //     let timeNeeded = 0;
-    //     let currManager = manager[empID];
-    //     timeNeeded = informTime[currManager];
-    //     while (currManager !== -1) {
-    //         currManager = manager[currManager];
-    //         timeNeeded += informTime[currManager];
-    //     }
-    //     return timeNeeded;
-    // }
+    function findInformTime(empID) {
+        if (empID === headID) return 0;
+        let timeNeeded = 0;
+        let currManager = manager[empID];
+        timeNeeded = informTime[currManager];
+        while (manager[currManager] !== -1) {
+            currManager = manager[currManager];
+            timeNeeded += informTime[currManager];
+        }
+        return timeNeeded;
+    }
 }
-console.log(neededTime(7, 6, [1, 2, 3, 4, 5, 6, -1], [0, 6, 5, 4, 3, 2, 1]));
-console.log(neededTime(6, 2, [2, 2, -1, 2, 2, 2], [0, 0, 1, 0, 0, 0]));
-console.log(neededTime(1, 0, [-1], [0]));
+console.log(neededTime(7, 6, [1, 2, 3, 4, 5, 6, -1], [0, 6, 5, 4, 3, 2, 1])); //expected 21
+console.log(neededTime(6, 2, [2, 2, -1, 2, 2, 2], [0, 0, 1, 0, 0, 0])); //expected 1
+console.log(neededTime(1, 0, [-1], [0])); //expected 0
